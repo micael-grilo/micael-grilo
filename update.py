248b5ccf034e59ec59f0306e4746d02e8b252fbb
Micael Grilo <micaelf@outlook.com>
2021-09-24 01:02:28 +0100

    Update files

--- update.py
+++ update.py
@@ -4,22 +4,18 @@ from dateutil import relativedelta
 
 birthDate = datetime(1991, 7, 4)
 currentDate = datetime(
-  datetime.today().year,
-  datetime.today().month, 
-  datetime.today().day
+    datetime.today().year,
+    datetime.today().month,
+    datetime.today().day
 )
 delta = relativedelta.relativedelta(currentDate, birthDate)
 
-def formatNumeralConcord():
-    concord = []
-    concord.append('month') if delta.months <= 1 else concord.append('months')
-    concord.append('day') if delta.days <= 1 else concord.append('days')
-    return concord
 
 def formatUptimeString():
-    days = formatNumeralConcord()[1]
-    months = formatNumeralConcord()[0]
-    return 'Uptime: "{} years, {} {}, {} {}"\n'.format(delta.years, delta.months, months, delta.days, days)
+    days = 'day' if delta.days <= 1 else 'days'
+    months = 'month' if delta.months <= 1 else 'months'
+    return f'Uptime: "{delta.years} years, {delta.months} {months}, {delta.days} {days}"\n'
+
 
 def overwriteFile():
     with open('README.md', 'r+') as file:
